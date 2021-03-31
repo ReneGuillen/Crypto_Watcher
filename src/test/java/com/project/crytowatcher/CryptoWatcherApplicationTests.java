7632c1b61039166791c682e6afa0a68b9aeac6ac
Guillen Lopez <renguill@amazon.com>
2021-03-31 13:34:14 -0500

    First Commit

---
+++ src/test/java/com/project/crytowatcher/CryptoWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crytowatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
