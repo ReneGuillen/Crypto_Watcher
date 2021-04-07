6b261f392b60f612c4329891ccd6908bdf5b2486
ReneGuillen <62676755+ReneGuillen@users.noreply.github.com>
2021-04-07 10:10:15 -0500

    API Controllers Updated

--- src/main/Back_End/UserController.java
+++ src/main/Back_End/UserController.java
@@ -18,35 +18,6 @@ public class UserController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     
-    @GetMapping("/getUser")
-    public String getUser(Principal principal) {
-        /*
-        Search Method to get username from github response after login to
-        later add the user to database table utilizing O(N)Time / O(N)Space.
-        Returns:
-            user's username as a string.
-        */
-        
-        //Search for username on principal's response
-        int count = 2;
-        int index = 0;
-        String details = principal.toString();
-        StringBuilder username = new StringBuilder();
-        
-        //get username's index in string.
-        while(count > 0){
-            if(details.charAt(index++) == '=')
-                count--;
-        }
-        //append chars to the stringbuilder to get full username.
-        for(int i = 0; i < details.length(); i++){
-            if(details.charAt(index) == ',')
-                break;
-            username.append(details.charAt(index++));
-        }
-        return username.toString();
-    }
-
     @PostMapping("/addUser")
     public @ResponseBody String addUser(Principal principal){
         /*
@@ -66,6 +37,33 @@ public class UserController {
             return "new user has been added '" + username + "' Welcome";
         }
         return "User " + username + " already exists, WELCOME BACK!!";
+    }
+    
+    @GetMapping("/getUser")
+    public String getUser(Principal principal) {
+        /*
+        Search Method to get username from github response after login to
+        later add the user to database table utilizing O(N)Time / O(N)Space.
+        Returns:
+            user's username as a string.
+        */
+        
+        //Search for username on principal's response
+        String details = principal.toString();
+        StringBuilder username = new StringBuilder();
+        
+        //get username's index in string.
+        while(count > 0){
+            if(details.charAt(index++) == '=')
+                count--;
+        }
+        //append chars to the stringbuilder to get full username.
+        for(int i = 0; i < details.length(); i++){
+            if(details.charAt(index) == ',')
+                break;
+            username.append(details.charAt(index++));
+        }
+        return username.toString();
     }
 
     //Check if user already exists on databseb table based on response/error.
