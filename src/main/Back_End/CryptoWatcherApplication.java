416e5b2a63b10f2086f7bb07cf77330abf2da4ca
ReneGuillen <62676755+ReneGuillen@users.noreply.github.com>
2021-04-07 09:11:30 -0500

    Back_End commit

--- src/main/Back_End/CryptoWatcherApplication.java
+++ src/main/Back_End/CryptoWatcherApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class CryptoWatcherApplication {
 	
-	//Start point of the application (Main Class).
+	//Starting point of the application.
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoWatcherApplication.class, args);
 	}
